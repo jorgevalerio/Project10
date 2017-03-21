817f8bdf95b56a687dd44210093cb464d8b41e59
jorgevalerio <jorgevalerio921@gmail.com>
2017-03-20 23:42:24 -0700

    Ready for Peer Review

--- js/ajax.js
+++ js/ajax.js
@@ -1,166 +1,133 @@
 
 $(document).ready(function() { 
 				  
-$('form').submit(function(evt) {
-	evt.preventDefault();
-	let ID = $('#pokeIdNumber').val();
-	let pokeURL = "http://pokeapi.co/api/v2/pokemon/" + ID +"/" ;	
+	$('form').submit(function(evt) {
+		evt.preventDefault();
+		let ID = $('#pokeIdNumber').val();
+		let pokeURL = "http://pokeapi.co/api/v2/pokemon/" + ID +"/" ;	
 
-	if (ID <= 699) {				  
-		function pokemonInfo(dataFromApi){
-		let pokeName     = dataFromApi.name;
-		let pokeImageUrl = dataFromApi.sprites.front_default;
-		let pokeType     = dataFromApi.types[0].type.name;
-			
-			
-		var pokeInfo = '<div>';
-			pokeInfo += '<h3> Info: </h3>';
-			pokeInfo += '<ul>';
-			pokeInfo += '<li> <strong>Name: </strong>  ' + pokeName + '</li>';
-			pokeInfo += '<li><strong> Type: </strong>  ' + pokeType +'</li>';
-			pokeInfo += '</ul>';
-			pokeInfo += '<img src="' + pokeImageUrl + '">';
-			
-			$('.pokeInfo').html(pokeInfo);
+		if (ID <= 699) {				  
+			function pokemonInfo(dataFromApi){
+				let pokeName     = dataFromApi.name;
+				let pokeImageUrl = dataFromApi.sprites.front_default;
+				let pokeType     = dataFromApi.types[0].type.name;
 
-		console.log(pokeName);
-		}
-	} else {
-			alert('There is only 699 species of Pokemon -Please enter a number up to 699');
-		}
 
-	$.getJSON(pokeURL,pokemonInfo);		
-	
-		});
+				var pokeInfo = '<div>';
+					pokeInfo += '<h3> Info: </h3>';
+					pokeInfo += '<ul>';
+					pokeInfo += '<li> <strong>Name: </strong>  ' + pokeName + '</li>';
+					pokeInfo += '<li><strong> Type: </strong>  ' + pokeType +'</li>';
+					pokeInfo += '</ul>';
+					pokeInfo += '<img src="' + pokeImageUrl + '">';
+
+					$('.pokeInfo').html(pokeInfo);
+
+				console.log(pokeName);
+			}
+		} else {
+				alert('There is only 699 species of Pokemon -Please enter a number up to 699');
+			}
+
+		$.getJSON(pokeURL,pokemonInfo);		
+
+	});
 		
 });
 
-
 $(document).ready(function() { 
 
-$("a").click(function() {
-    return false;
-})
-
-});
-///////////////////////////////////////////////////////////////////////
-
-// pokemon List 20 
-
-///////////////////////////////////////////////////////////////////////
-
-
-
-
-$(document).ready(function() { 
-				  
-$('button.buttonPokemonList').click(function(evt) {
-	evt.preventDefault();
-	
-	let pokeURList = "http://pokeapi.co/api/v2/pokemon/";	
-
-					  
-		function pokemonInfoList(dataFromApi){
-		var pokeName     = dataFromApi.count;
-		let pokemonURL     = dataFromApi.count;
+	$('button.buttonPokemonList').click(function(evt) {
+			evt.preventDefault();
+			let pokeURList = "http://pokeapi.co/api/v2/pokemon/";	
 		
-		let pokeListHTML = '<div id="ok"><ul>';
-		
-		$.each(dataFromApi.results, function (i, pokeMon){
-			
-			   pokeListHTML += '<li>';
-			   pokeListHTML += pokeMon.name;
-			  
-			   pokeListHTML += '<button> <a href="'+pokeMon.url+ '">Learn More</a>';
-			   pokeListHTML += '</button>';
-			   pokeListHTML += '</li>';
+			function pokemonInfoList(dataFromApi){
+				var pokeName     = dataFromApi.count;
+				let pokemonURL     = dataFromApi.count;
+				let pokeListHTML = '<div id="ok"><ul>';
+				
+				$.each(dataFromApi.results, function (i, pokeMon){
 
-			console.log(pokeMon.name);
-		});
-			
-			
-			
+					pokeListHTML += '<li>';
+					pokeListHTML += pokeMon.name;
+					pokeListHTML += '<button> <a href="'+pokeMon.url+ '">Learn More</a>';
+					pokeListHTML += '</button>';
+					pokeListHTML += '</li>';
+
+					console.log(pokeMon.name);
+				});
+
 			  pokeListHTML += '</ul></div>';
-			
-			
-		
-			$('#pokemonList').html(pokeListHTML);
-			
-			
-	
-  $('a').click(function(stop){
-	  stop.preventDefault();
-	  
-	 var urlPokeSolo = $(this).attr("href");
-	  var modal = document.getElementById('myModal');
-	  var span = document.getElementsByClassName("close")[0];
-	  
-	  function openbox() {
-    modal.style.display = "block";
-}
-	  
-	  openbox();
-	  
-	  span.onclick = function() {
-    modal.style.display = "none";
-};
+
+				$('#pokemonList').html(pokeListHTML);
+
+					$('a').click(function(stop){
+					  stop.preventDefault();
+
+					  var urlPokeSolo = $(this).attr("href");
+					  var modal = document.getElementById('myModal');
+					  var span = document.getElementsByClassName("close")[0];
+
+					  function openbox() {
+					modal.style.display = "block";
+						  $('.pokeInfo2').html("loading...");
+				}
+
+					  openbox();
+
+					  span.onclick = function() {
+					modal.style.display = "none";
+				};
+
+					  console.log(urlPokeSolo);
 	 
-	  console.log(urlPokeSolo);
-	  
-	  
-	  // third API Call will go here
-	  
-	  let pokeURL = urlPokeSolo ;	
-	  
-	  function pokemonInfo(dataFromApi){
-		let pokeName     = dataFromApi.name;
-		let pokeImageUrl = dataFromApi.sprites.front_default;
-		let pokeType     = dataFromApi.types[0].type.name;
-			
-			
-		var pokeInfo = '<div>';
-			pokeInfo += '<h3> Info: </h3>';
-			pokeInfo += '<ul>';
-			pokeInfo += '<li> <strong>Name: </strong>  ' + pokeName + '</li>';
-			pokeInfo += '<li><strong> Type: </strong>  ' + pokeType +'</li>';
-			pokeInfo += '</ul>';
-			pokeInfo += '<img src="' + pokeImageUrl + '">';
-			
-			$('.pokeInfo2').html(pokeInfo);
 
-		console.log(pokeName);
-		}
-	  
-	  $.getJSON(pokeURL,pokemonInfo);	
-	  
-	  ////////////
-	  
-	  window.onclick = function(event) {
-    if (event.target == modal) {
-		$('.pokeInfo2').html("1");
-        modal.style.display = "none";
-		
-    }
-};
-	  
-	  
-	  
-  });
-			
-			
-			
-			
+					  let pokeURL = urlPokeSolo ;	
 
-	
+					  function pokemonInfo(dataFromApi){
+						let pokeName     = dataFromApi.name;
+						let pokeWeight     = dataFromApi.weight;
+						let pokeHeight     = dataFromApi.height;
+						let pokeBaseExperience     = dataFromApi.base_experience;
+						let pokeImageUrl = dataFromApi.sprites.front_default;
+						let pokeType     = dataFromApi.types[0].type.name;
+
+
+						var pokeInfo = '<div>';
+							pokeInfo += '<h3> Info: </h3>';
+							pokeInfo += '<ul>';
+							pokeInfo += '<li> <strong>Name: </strong>  ' + pokeName + '</li>';
+							pokeInfo += '<li><strong> Type: </strong>  ' + pokeType +'</li>';
+						    pokeInfo += '<li><strong> Weight: </strong>  ' + pokeWeight  +'</li>';
+						  	pokeInfo += '<li><strong> BE: </strong>  ' + pokeBaseExperience+'</li>';
+						  pokeInfo += '<li><strong> Height: </strong>  ' + pokeHeight+'</li>';
+							pokeInfo += '</ul>';
+							pokeInfo += '<img src="' + pokeImageUrl + '">';
+
+							$('.pokeInfo2').html(pokeInfo);
+
+						console.log(pokeName);
+						}
+
+					  $.getJSON(pokeURL,pokemonInfo);	
+
+
+					  window.onclick = function(event) {
+					if (event.target == modal) {
+						$('.pokeInfo2').html("loading...");
+						modal.style.display = "none";
+
+					}
+				};
+
+				  });
 		}
-	
-	
+
+
 
 	$.getJSON(pokeURList,pokemonInfoList);	
-	
 
-	
-		});
+	});
 		
 });
 
